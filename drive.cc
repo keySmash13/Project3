51a6df7f5ae3cd26bdd37368b44a2befb1318c26
keySmash13 <holdcraa@email.sc.edu>
2025-04-29 21:08:07 -0400

    adding tests

--- drive.cc
+++ drive.cc
@@ -84,28 +84,97 @@ int main() {
   cout << "Printing copy string set:" << endl;
   copy_string_set.Print();
 
-  cout << endl << "    IsSubsetOf tests:" << endl;
+  cout << endl << "    IsSubsetOf/IsSupersetOf tests:" << endl;
   cout << "Int set 1:";
   int_set.Print();
   cout << "Int set 2:";
   duplicate_int_set.Print();
   cout << "set1.IsSubsetOf(set2) test, expected true (1): "
   << int_set.IsSubsetOf(duplicate_int_set) << endl;
+  cout << "set2.IsSupersetOf(set1) test, expected true (1): "
+  << duplicate_int_set.IsSupersetOf(int_set) << endl;
   cout << "Char set 1:";
   char_set.Print();
   cout << "Char set 2:";
   duplicate_char_set.Print();
   cout << "set1.IsSubsetOf(set2) test, expected true (1): "
   << char_set.IsSubsetOf(duplicate_char_set) << endl;
+  cout << "set2.IsSupersetOf(set1) test, expected true (1): "
+  << duplicate_char_set.IsSupersetOf(char_set) << endl;
   cout << "String set 1:";
   string_set.Print();
   cout << "String set 2:";
   copy_string_set.Print();
   cout << "set1.IsSubsetOf(set2) test, expected true (1): "
   << char_set.IsSubsetOf(duplicate_char_set) << endl;
+  cout << "set2.IsSupersetOf(set1) test, expected true (1): "
+  << copy_string_set.IsSupersetOf(string_set) << endl;
 
-  cout << endl << "    IsSupersetOf tests:" << endl;
-//TODO
+  cout << endl << "    RemoveDuplicate tests:" << endl;
+  cout << "Removing duplicates from character set:" << endl << "Initial: ";
+  duplicate_char_set.Print();
+  duplicate_char_set.RemoveDuplicates();
+  cout << "Final: ";
+  duplicate_char_set.Print();
 
+  // bool = true, bool = false
+  cout << endl << "    Concat tests:" << endl;
+  cout << "Concatting character sets, including duplicate values:" << endl << "Set 1:";
+  duplicate_char_set.Print();
+  cout << "Set 2: ";
+  char_set.Print();
+  duplicate_char_set.Concat(char_set);
+  cout << "Concatenated: ";
+  duplicate_char_set.Print();
+  cout << "Concatting string sets, ignoring duplicate values:" << endl << "Set 1:";
+  string_set.Print();
+  cout << "Set 2: ";
+  copy_string_set.Print();
+  string_set.Concat(copy_string_set, false);
+  cout << "Concatenated: ";
+  string_set.Print();
+
+  // = == != += -= -
+  cout << endl << "    Operator tests:" << endl;
+  // =
+  cout << "Testing assignment operator:" << endl << "Original set: ";
+  int_set.Print();
+  cout << "Assigning original set to new set:" << endl;
+  MySet<int> assigned_int_set;
+  assigned_int_set = int_set;
+  assigned_int_set.Print();
+  // ==
+  cout << "Testing == operator, expected true (1): "
+  << (int_set == assigned_int_set) << endl;
+  // !=
+  cout << "Testing != operator, expected false (0): "
+  << (int_set != assigned_int_set) << endl;
+  // +=
+  cout << "Testing += operator:" << endl;
+  int_set += assigned_int_set;
+  int_set.Print();
+  // -=
+  cout << "Testing -= operator:" << endl;
+  int_set -= assigned_int_set;
+  int_set.Print();
+  // -
+  cout << "Testing - operator, expected {}:" << endl;
+  int_set = int_set - assigned_int_set;
+  int_set.Print();
+
+  cout << endl << "    Intersection tests:" << endl;
+  string str_arr_1[7] = {"I", "just", "made", "a", "new", "string", "set"};
+  MySet<string> str_set_1(str_arr_1, 7);
+  str_set_1.Print();
+  string str_arr_2[8] = {"And", "this", "string", "set", "is", "a", "different", "set"};
+  MySet<string> str_set_2(str_arr_2, 8);
+  str_set_1.Print();
+  cout << "Creating Intersection:" << endl;
+  str_set_1.Intersection(str_set_2);
+  str_set_1.Print();
+
+  // TODO
+  cout << endl << "    Sort tests:" << endl;  // Ascending and Descending
+  
   return 0;
 }
